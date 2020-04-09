dea4b919bd73a51551b7c645c457bcc3f8bfac29
mateo-a <alexander.urrego@gmail.com>
2020-04-09 18:02:54 -0500

    [ADD] Add Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
-""" Compress before sending """
+"""
+Fabric script that generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack.
+"""
 from fabric.api import *
-from datetime import datetime
+import time
 
 
 def do_pack():
     """
-    Generates a .tgz archive from the contents
-    of the web_static folder
+    Function do_pack must return the archive path if the archive has been
+    correctly generated. Otherwise, it should return None
     """
+    the_time = time.strftime("%Y%m%d%H%M%S")
     try:
-        today = datetime.now().strftime('%Y%m%d%H%M%S')
-        path = "versions/web_static_{:s}.tgz".format(today)
-
-        msg1 = "Packing web_static to {:s}".format(path)
-        print(msg1)
-
-        with hide('running'):
-            local('mkdir -p ./versions')
-
-        local('tar -cvzf {:s} web_static'.format(path))
-
-        with hide('running'):
-            size = local('wc -c < {:s}'.format(path), capture=True)
-
-        msg2 = 'web_static packed: {:s} -> {:s}Bytes'.format(path, size)
-        print(msg2)
-
-        return path
-
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(the_time))
+        return ("versions/web_static_{}.tgz".format(the_time))
     except:
         return None
