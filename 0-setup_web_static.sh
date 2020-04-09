2caaa6ef5021dbd77abc55ac31cbee8203e97384
mateo-a <alexander.urrego@gmail.com>
2020-04-09 17:11:34 -0500

    [ADD] Add Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-# Prepare Web Server
-apt-get update -y
-apt-get install nginx -y
+# script that sets up web servers for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
@@ -13,4 +12,3 @@ loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\taut
 
 sudo sed -i "38i $loc" /etc/nginx/sites-available/default
 service nginx restart
-exit 0
