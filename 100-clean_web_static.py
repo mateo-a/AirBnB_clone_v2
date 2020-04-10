30010ee186fff18c7df1f850239a45c03e87a6f8
mateo-a <alexander.urrego@gmail.com>
2020-04-09 22:53:08 -0500

    [FIX] Task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -64,17 +64,11 @@ def do_clean(number=0):
     """
     deletes out-of-date archives, using the function do_clean
     """
-    if number == 0 or number == 1:
-        with cd.local('./versions/'):
-            local("ls -lv | rev | cut -f 1 | rev | \
-            head -n +1 | xargs -d '\n' rm -rf")
-        with cd('/data/web_static/releases/'):
-            run("sudo ls -lv | rev | cut -f 1 | \
-            rev | head -n +1 | xargs -d '\n' rm -rf")
+    if number == 0:
+        number = 2
     else:
-        with cd.local('./versions/'):
-            local("ls -lv | rev | cut -f 1 | rev | \
-            head -n +{} | xargs -d '\n' rm -rf".format(number))
-        with cd('/data/web_static/releases/'):
-            run("sudo ls -lv | rev | cut -f 1 | \
-            rev | head -n +{} | xargs -d '\n' rm -rf".format(number))
+        number = number + 1
+    with lcd('versions'):
+        local('ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    with cd('/data/web_static/releases'):
+        run('ls -t | tail -n +{} | xargs rm -rf'.format(number))
