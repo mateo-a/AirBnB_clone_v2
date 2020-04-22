a015a7aeefb51ccc8f351364ca80e2c8122351f6
mateo-a <alexander.urrego@gmail.com>
2020-04-22 14:53:58 -0500

    [ADD] ADD task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+        """ Displays "Hello HBNB """
+        return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+        """ Displays HBNB """
+        return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+        """  Display “C ” followed by the value """
+        return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', stric_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is_cool'):
+        """ display “Python ”, followed by the value """
+        return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+        """ Main function """
+        app.run(host="0.0.0.0", port=5000)
