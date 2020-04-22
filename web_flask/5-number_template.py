1f0a53515488221b186d5bce0b8b6d17823a9148
mateo-a <alexander.urrego@gmail.com>
2020-04-22 16:33:35 -0500

    [FIX] FIX task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application """
 from flask import Flask
-froma flask import render_template
+from flask import render_template
 app = Flask(__name__)
 
 
