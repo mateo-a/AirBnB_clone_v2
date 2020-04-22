967f47390c7ee56e75d46e3f3d39d1d69e8add64
mateo-a <alexander.urrego@gmail.com>
2020-04-22 18:47:54 -0500

    [FIX] Fix task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,7 +15,7 @@ def close_session(error):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ display a HTML page: (inside the tag BODY) """
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     return render_template("7-states_list.html", states=states)
 
