387dbf8fc10fbfe8b3772815205086d6be162400
mateo-a <alexander.urrego@gmail.com>
2020-04-22 15:01:17 -0500

    [FIX] Fix task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,7 +22,7 @@ def c_text(text):
         return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', stric_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is_cool'):
         """ display “Python ”, followed by the value """
